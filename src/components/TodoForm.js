e5c7f4fe36116cc5a0207426d9ce19279fc6106b
veeveck <veeveckjeet@gmail.com>
2022-08-17 22:28:55 -0400

    Final Todo git push git commit -m "Final Todo"

---
+++ src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TodoForm = ({handleSubmit,editId,todo,setTodo}) => {
+  return (
+    <form className='todoForm' onSubmit={handleSubmit}>
+        <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)}/>
+        <button type="submit">{editId?"Edit" : "Go"}</button>
+    </form>
+  )
+}
+
+export default TodoForm
---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TodoList = ({handleDelete,handleEdit,todos}) => {
+  return (
+    <ul className='allTodos'>
+         {todos.map((t)=>(
+        <li className='singleTodo' key ={t.id}>
+           <span className='todo' >{t.todo}</span>
+           <button onClick={()=>handleEdit(t.id)}>Edit</button>
+           <button onClick={()=>handleDelete(t.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default TodoList
